adaa097883bfa7b5eb7d4884b865b07d61875b83
andkir1024 <andkir@mail.ru>
2023-11-21 13:33:20 +0300

    update

---
+++ chatGPT.py
@@ -0,0 +1,11 @@
+# from langchain.chat_models import GigaChat
+# from langchain.prompts.chat import (
+#     ChatPromptTemplate,
+#     SystemMessagePromptTemplate,
+#     AIMessagePromptTemplate,
+#     HumanMessagePromptTemplate,
+# )
+# from langchain.schema import AIMessage, HumanMessage, SystemMessage
+
+# # Авторизация в сервисе GigaChat
+# chat = GigaChat(credentials="MTI3YzYzN2ItOGIwOC00NDNiLWJmOGItOGM3N2NmNTYxMjZhOjVjYWYxMjljLWJlMjEtNDQ4Yi05M2Q5LTI1N2ZhMmEzMmU2Mw==", verify_ssl_certs=False)
