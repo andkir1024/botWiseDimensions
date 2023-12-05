6727f62d1a929f11dbb3b9bceca7175c24ddfdc1
andkir1024 <andkir@mail.ru>
2023-12-05 08:04:30 +0300

    update

---
+++ textUtilty.py
@@ -0,0 +1,21 @@
+class textUtility:
+    # ограничить ответ до первого вхождения слов  borderKey
+    def rangeAnswer(msg, borderKey):
+        index = msg.lower().find(borderKey.lower())
+        if index > 0:
+            return msg[:index]
+        return msg
+    # подготовка ответа на вывод удалениее ненужных слов
+    def prepareAnswer(msg):
+        index = msg.lower().find(":")
+        if index > 0:
+            msgNex = msg[index+1:]
+            
+            index = msgNex.lower().find("Правильный ответ:".lower())
+            if index > 0:
+                msgNex = msgNex[:index]
+            index = msgNex.lower().find("Ответ:".lower())
+            if index > 0:
+                msgNex = msgNex[:index]
+            return msgNex
+        return msg
